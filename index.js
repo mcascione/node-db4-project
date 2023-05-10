eec025ddfa019b8a5f5d54076c16bcdb3784f52a
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-10 15:55:49 -0700

    wired up basic server

---
+++ index.js
@@ -0,0 +1,12 @@
+const server = require("./server");
+const env = require("dotenv").config();
+
+const port = process.env.PORT || 9000;
+
+server.use("*", (req, res) => {
+  res.status(404).send(`404 Error: Page does not exist`);
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/recipes', (req, res) => {
+    res.send('Recipes go here!')
+})
+
+module.exports = server;
