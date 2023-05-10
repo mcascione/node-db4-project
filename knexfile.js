99157918ed75d5e504fd90b1e887ce06988c6d70
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-10 16:04:17 -0700

    add knex config and skeleton schema migration

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/recipes.db3",
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+  },
+};
