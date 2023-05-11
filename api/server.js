f391013386452e91aca8a478c39bbae72d541c8d
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-10 17:40:52 -0700

    built get function for recipes table in server.js

--- api/server.js
+++ api/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
-
+const db = require('../data/db.config');
 const server = express();
 
 server.use(express.json());
 
-
-
-server.get('/api/recipes', (req, res) => {
-    res.send('Recipes go here!')
-})
+server.get("/api/recipes", async (req, res) => {
+  try {
+    const recipes = await db('recipes')
+    res.status(200).json(recipes);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      customMessage: "not able to fetch your recipes",
+    });
+  }
+});
 
 module.exports = server;
